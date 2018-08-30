8abcd8b771ff048e4b794d65e57cd5d86a9f3816
wayne.smith <wayne.smith@sage015358.sageukie.adinternal.com>
2018-08-30 13:27:11 +0100

    - start of templates

---
+++ templates/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while getopts ":t:p:" opt; do
+  case $opt in
+    t) TYPE="$OPTARG";;
+    p) PACKAGE_NAME="$OPTARG";;
+    # optional parameters
+    \?) echo "Invalid option -$OPTARG" >&2;;
+  esac
+done
+
+if [ $TYPE == "internal" ]; then
+    PACKAGE_NAME="internal-$PACKAGE_NAME"
+fi
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')][TEMPLATE] generating $TYPE package for $PACKAGE_NAME"
+
+FOLDER_NAME="${PACKAGE_NAME/-/.}"
+cp -R "$PWD/templates/$TYPE" "$PWD/packages/$FOLDER_NAME"
+
+find "$PWD/packages/$FOLDER_NAME" -name '*.*' -exec sed -i -e 's/__package_name__/$PACKAGE_NAME/g' {} \;
