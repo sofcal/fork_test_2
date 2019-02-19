c79417c029058c6a3ee6b95ae5f87ae83fe9fc60
graham.postles <graham.postles@sage.com>
2019-02-19 12:02:31 +0000

    update units so thay work with Marks requested changes

--- packages/cloudFront.securityHeaders/test/unit/impl.spec.js
+++ packages/cloudFront.securityHeaders/test/unit/impl.spec.js
@@ -26,12 +26,11 @@ describe.only('impl', function(){
             'cache-control': [{ key: 'Cache-Control', value: 'public, max-age=86400, no-transform' }],
             'strict-transport-security': [{ key: 'Strict-Transport-Security', value: 'max-age=15552000; includeSubDomains' }],
             'x-content-security-policy': [{ key: 'X-Content-Security-Policy', value: 'allow-from frame-ancestors https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz' }],
-            'content-security-policy-report-only': [{   key: 'Content-Security-Policy-Report-Only', value: 'img-src \'self\' https://www.google-analytics.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://cdnjs.cloudflare.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; report-uri slippydog.com' }],
+            'content-security-policy': [{ key: 'Content-Security-Policy', value: 'img-src \'self\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://cdn.plaid.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://fonts.gstatic.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; '}],
             'x-xss-protection': [{ key: 'X-XSS-Protection', value: '1; mode=block' }],
             'x-content-type-options': [{ key: 'X-Content-Type-Options', value: 'nosniff' }],
             'x-download-options': [{ key: 'X-Download-Options', value: 'noopen' }],
             'x-permitted-cross-domain-policies': [{ key: 'X-Permitted-Cross-Domain-Policies', value: 'master-only' }],
-            'x-frame-options': [{ key: 'X-Frame-Options', value: 'DENY' }],
             'server': [{ key: 'Server', value: '' }],
             'feature-policy': [{ key: 'Feature-Policy', value: 'geolocation \'none\'; midi \'none\'; sync-xhr \'self\'; microphone \'none\'; camera \'none\'; magnetometer \'none\'; gyroscope \'none\'; speaker \'none\'; fullscreen \'self\'; payment \'self\'' }],
             'referrer-policy': [{ key: 'Referrer-Policy', value: 'same-origin' }]
@@ -69,7 +68,7 @@ describe.only('impl', function(){
             'cache-control': [{ key: 'Cache-Control', value: 'public, max-age=86400, no-transform' }],
             'strict-transport-security': [{ key: 'Strict-Transport-Security', value: 'max-age=15552000; includeSubDomains' }],
             'x-content-security-policy': [{ key: 'X-Content-Security-Policy', value: 'allow-from frame-ancestors https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz' }],
-            'content-security-policy-report-only': [{   key: 'Content-Security-Policy-Report-Only', value: 'img-src \'self\' https://www.google-analytics.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://cdnjs.cloudflare.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; report-uri slippydog.com' }],
+            'content-security-policy': [{ key: 'Content-Security-Policy', value: 'img-src \'self\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://cdn.plaid.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://fonts.gstatic.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; '}],
             'x-xss-protection': [{ key: 'X-XSS-Protection', value: '1; mode=block' }],
             'x-content-type-options': [{ key: 'X-Content-Type-Options', value: 'nosniff' }],
             'x-download-options': [{ key: 'X-Download-Options', value: 'noopen' }],
@@ -93,13 +92,13 @@ describe.only('impl', function(){
             })
     });
 
-    it('should add the x-frame-options-deny if the query string does not contain a domain in the whitelist', () => {
+    it('should add the x-frame-options-deny if the query string does not contain a domain in the whitelist and the uri contains xframe.html', () => {
         const logFunc = (data) => {console.log(JSON.stringify(data))};
         const expectedHeaders = {
             'cache-control': [{ key: 'Cache-Control', value: 'public, max-age=86400, no-transform' }],
             'strict-transport-security': [{ key: 'Strict-Transport-Security', value: 'max-age=15552000; includeSubDomains' }],
             'x-content-security-policy': [{ key: 'X-Content-Security-Policy', value: 'allow-from frame-ancestors https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz' }],
-            'content-security-policy-report-only': [{   key: 'Content-Security-Policy-Report-Only', value: 'img-src \'self\' https://www.google-analytics.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://cdnjs.cloudflare.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; report-uri slippydog.com' }],
+            'content-security-policy': [{ key: 'Content-Security-Policy', value: 'img-src \'self\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://cdn.plaid.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://fonts.gstatic.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; '}],
             'x-xss-protection': [{ key: 'X-XSS-Protection', value: '1; mode=block' }],
             'x-content-type-options': [{ key: 'X-Content-Type-Options', value: 'nosniff' }],
             'x-download-options': [{ key: 'X-Download-Options', value: 'noopen' }],
@@ -111,7 +110,7 @@ describe.only('impl', function(){
         };
         const htmlBody = '<HEAD/><BODY>Test</BODY>';
         const bodyString = JSON.stringify(htmlBody);
-        const event = { logger: { info: logFunc, error: logFunc }, Records: [{ cf: { request: { uri: 'https://www.test.com' ,querystring: 'xframedomain=https://doesnotexist.com'}, response: { headers: {}, body: bodyString } }}]};
+        const event = { logger: { info: logFunc, error: logFunc }, Records: [{ cf: { request: { uri: 'https://www.test.com/xframe.html', querystring: 'xframedomain=https://doesnotexist.com'}, response: { headers: {}, body: bodyString } }}]};
         let params = {};
         params['outboundApi.callbackUriWhiteList'] = whiteList;
 
@@ -122,13 +121,13 @@ describe.only('impl', function(){
             })
     });
 
-    it('should add the x-frame-options-deny if the query string does not contain a domain', () => {
+    it('should add the x-frame-options-deny if the query string does not contain a domain and the uri contains xframe.html', () => {
         const logFunc = (data) => {console.log(JSON.stringify(data))};
         const expectedHeaders = {
             'cache-control': [{ key: 'Cache-Control', value: 'public, max-age=86400, no-transform' }],
             'strict-transport-security': [{ key: 'Strict-Transport-Security', value: 'max-age=15552000; includeSubDomains' }],
             'x-content-security-policy': [{ key: 'X-Content-Security-Policy', value: 'allow-from frame-ancestors https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz' }],
-            'content-security-policy-report-only': [{   key: 'Content-Security-Policy-Report-Only', value: 'img-src \'self\' https://www.google-analytics.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://cdnjs.cloudflare.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; report-uri slippydog.com' }],
+            'content-security-policy': [{ key: 'Content-Security-Policy', value: 'img-src \'self\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://s3-eu-west-1.amazonaws.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; script-src \'self\' *.sage.com \'unsafe-inline\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://cdn.plaid.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://fonts.gstatic.com https://pre-addons.eu.sageone.com https://pre-addons.na.sageone.com https://pre-addons.sageone.biz; '}],
             'x-xss-protection': [{ key: 'X-XSS-Protection', value: '1; mode=block' }],
             'x-content-type-options': [{ key: 'X-Content-Type-Options', value: 'nosniff' }],
             'x-download-options': [{ key: 'X-Download-Options', value: 'noopen' }],
@@ -140,7 +139,7 @@ describe.only('impl', function(){
         };
         const htmlBody = '<HEAD/><BODY>Test</BODY>';
         const bodyString = JSON.stringify(htmlBody);
-        const event = { logger: { info: logFunc, error: logFunc }, Records: [{ cf: { request: { uri: 'https://www.test.com' ,querystring: 'dummy=dummy'}, response: { headers: {}, body: bodyString } }}]};
+        const event = { logger: { info: logFunc, error: logFunc }, Records: [{ cf: { request: { uri: 'https://www.test.com/xframe.html' ,querystring: 'dummy=dummy'}, response: { headers: {}, body: bodyString } }}]};
         let params = {};
         params['outboundApi.callbackUriWhiteList'] = whiteList;
 
@@ -194,6 +193,7 @@ describe.only('impl', function(){
         const logs = [];
         const expexctedLogs = [
             '{"function":"impl.run","log":"started"}',
+            '{"function":"impl.run","log":"request","params":{"request":{"uri":"https://www.test.com/xframe.html","querystring":""}}}',
             '{"function":"impl.run","log":"Adding static security headers"}',
             '{"function":"impl.run","log":"added x-frame-options allow-from for domain","params":{"xFrameOptions":"default-src \'none\'"}}',
             '{"function":"impl.run","log":"query","params":{"query":""}}',
@@ -205,7 +205,7 @@ describe.only('impl', function(){
 
         const htmlBody = '<HEAD/><BODY>Test</BODY>';
         const bodyString = JSON.stringify(htmlBody);
-        const event = { logger: { info: logFunc, error: logFunc }, Records: [{ cf: { request: { uri: 'https://www.test.com' ,querystring: ''}, response: { headers: {}, body: bodyString } }}]};
+        const event = { logger: { info: logFunc, error: logFunc }, Records: [{ cf: { request: { uri: 'https://www.test.com/xframe.html' ,querystring: ''}, response: { headers: {}, body: bodyString } }}]};
         let params = {};
 
         return impl.run(event, params)
