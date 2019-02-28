b65aa4c5273a3c4ee00ce1e7a74ded55da06c2cd
Neil Lupton <neil.lupton@sage.com>
2019-02-28 11:32:13 +0000

    internal.jwt.authenticator start

---
+++ packages/internal.jwt.authenticator/test/int/index.spec.js
@@ -0,0 +1,8 @@
+const should = require('should');
+
+describe('internal-jwt-authenticator', function(){
+    // placeholder
+    it('should force file to be evaluated', (done) => {
+        done();
+    });
+});
