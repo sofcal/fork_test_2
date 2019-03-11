888a6bd930a99a85af931be44c64faece138df95
Blaum <marton.blaum@sage016712.sageukie.adinternal.com>
2019-03-11 12:55:09 +0000

    Fixing files for ESLint

--- packages/lambda.scheduled.keyRotation/lib/dbQueries.js
+++ packages/lambda.scheduled.keyRotation/lib/dbQueries.js
@@ -1,8 +1,7 @@
 'use strict';
+
 const Promise = require('bluebird');
 const _ = require('underscore');
-const DB =  require('@sage/bc-services-db');
-const mongodb = require('mongodb');
 
 class DbQueries {
     constructor(db) {
@@ -13,68 +12,60 @@ class DbQueries {
         return getProductsImpl(this, ...args);
     }
 
-    getOrgsToRotate(...args){
-        return getOrgsToRotateImpl(this, ...args)
+    getOrgsToRotate(...args) {
+        return getOrgsToRotateImpl(this, ...args);
     }
 
-    updateOrgPrimaryToken(...args){
-        return updateOrgPrimaryTokenImpl(this, ...args)
+    updateOrgPrimaryToken(...args) {
+        return updateOrgPrimaryTokenImpl(this, ...args);
     }
 }
 
 const getProductsImpl = Promise.method((self) => {
-    return self.db.collection('Product').find().toArray()
+    return self.db.collection('Product').find().toArray();
 });
 
 const getOrgsToRotateImpl = Promise.method((self, excludedProductIds) => {
     const exclusionsQuery = [];
-    _.each(excludedProductIds,(productId) => {
-        exclusionsQuery.push({$ne: ['$$products.productId',productId]});
+    _.each(excludedProductIds, (productId) => {
+        exclusionsQuery.push({ $ne: ['$$products.productId', productId] });
     });
 
-    let aggrQuery = [
-        { $project :{
-            '_id' : 1,
-            'products': {
+    const aggrQuery = [
+        { $project: {
+            _id: 1,
+            products: {
                 $filter: {
                     input: '$products',
                     as: 'products',
-                    cond: { $and: exclusionsQuery}
+                    cond: { $and: exclusionsQuery }
                 }
             }
-          }
-        }
-        ,
-        {$unwind : '$products'}
-        ,
-        { '$project': {
-            '_id': 1,
-            'productId': '$products.productId',
-            'token': '$products.token',
-            'secondaryToken': '$products.secondaryToken',
-            'notRotating' : {$eq: ['$products.token', '$products.secondaryToken']}
-        }},
-        {$match : {'notRotating': true}}
-        ,
-        { '$project': {
-            '_id': 1,
-            'productId': 1,
-            'token': 1
-        }}
+        } },
+        { $unwind: '$products' },
+        { $project: {
+            _id: 1,
+            productId: '$products.productId',
+            token: '$products.token',
+            secondaryToken: '$products.secondaryToken',
+            notRotating: { $eq: ['$products.token', '$products.secondaryToken'] }
+        } },
+        { $match: { notRotating: true } },
+        { $project: {
+            _id: 1,
+            productId: 1,
+            token: 1
+        } }
     ];
 
     return self.db.collection('Organisation').aggregate(aggrQuery).toArray();
 });
 
-const updateOrgPrimaryTokenImpl = Promise.method((self, orgId, productId, oldToken, newToken, tokenTimeStamp) =>{
-    const query = { '_id' : orgId, 'products.productId' : productId, 'products.token': oldToken};
-    const update = { "$set": { "products.$.token": newToken, "products.$.tokenTimestamp": tokenTimeStamp}}
+const updateOrgPrimaryTokenImpl = Promise.method((self, orgId, productId, oldToken, newToken, tokenTimeStamp) => {
+    const query = { _id: orgId, 'products.productId': productId, 'products.token': oldToken };
+    const update = { $set: { 'products.$.token': newToken, 'products.$.tokenTimestamp': tokenTimeStamp } };
 
     return self.db.collection('Organisation').updateOne(query, update);
 });
 
 module.exports = DbQueries;
-
-
-
-
--- packages/lambda.scheduled.keyRotation/lib/handler.js
+++ packages/lambda.scheduled.keyRotation/lib/handler.js
@@ -4,83 +4,95 @@ const Promise = require('bluebird');
 const DbQueries = require('./dbQueries');
 const _ = require('underscore');
 const ParameterStoreStaticLoader = require('@sage/bc-parameterstore-static-loader');
+const BcServicesDb = require('@sage/bc-services-db');
+const crypto = require('crypto');
+const { RequestLogger } = require('@sage/bc-request-logger');
+
 const serviceImpls = {
-    DB: require('@sage/bc-services-db')
+    DB: BcServicesDb
 };
 
-let env, region, dbName, params;
+let env;
+let region;
+let dbName;
+let params;
+let logger;
 
 module.exports.run = (event, context, callback) => {
     const services = {};
-    console.log('event', event);
+    const func = 'handler.run';
+
+    // eslint-disable-next-line no-param-reassign
+    event.logger = RequestLogger.Create({ service: 'scheduled-keyRotation' }, event.logLevel);
+    ({ logger } = event);
+    event.logger.info({ function: func, log: `started with event ${event}` });
 
     return Promise.resolve(undefined)
         .then(() => {
-            env = event.env;
-            region = event.region;
+            ({ env } = event);
+            ({ region } = event);
             dbName = event.database;
 
-            if(!env || !region) {
+            if (!env || !region) {
                 throw new Error(`invalid parameters - env: ${env}; region: ${region};`);
             }
 
-            console.log(`starting request for - env: ${env}; region: ${region}`);
+            event.logger.info({ function: func, log: `starting request for - env: ${env}; region: ${region}` });
         })
         .then(() => getParams(env, region))
         .then((result) => {
             params = result;
-            return getServices(env, region, params, services)
+            return getServices(env, region, params, services);
         })
         .then(() => connectDB(services))
         .then((db) => {
-            return rotateOrgs(db,params);
+            return rotateOrgs(db, params);
         })
-        .then(()=>{
+        .then(() => {
             return services.db.disconnect()
-                .then(()=>{
+                .then(() => {
                     const response = {
                         statusCode: 200,
                         body: 'OK'
                     };
-                    console.log('invoke callback');
+                    event.logger.info({ function: func, log: 'invoke callback' });
                     callback(null, response);
-                })
+                });
         })
         .catch((err) => {
-            console.log('Failed with error:',err)
-        })
+            event.logger.error({ function: func, log: 'an error occurred while processing the request', error: err.message || err });
+        });
 };
 
-const rotateOrgs = Promise.method((db, params)=>{
+const rotateOrgs = Promise.method((db, _params) => {
+    const func = 'handler.rotateOrgs';
     const dbQueries = new DbQueries(db);
     let excludedProductIds;
-    return getExcludedProductIds(dbQueries,params)
-        .then((result)=>{
+    return getExcludedProductIds(dbQueries, _params)
+        .then((result) => {
             excludedProductIds = result;
-            console.log('excludedProductIds:', excludedProductIds);
+            logger.info({ function: func, log: `excludedProductIds: ${excludedProductIds}` });
         })
         .then(() => dbQueries.getOrgsToRotate(excludedProductIds))
         .then((orgRotateDetails) => {
-            return Promise.each(orgRotateDetails, (rotateDetails)=>{
-                console.log(`update token for Org:${rotateDetails._id} ProductId:${rotateDetails.productId}`);
+            return Promise.each(orgRotateDetails, (rotateDetails) => {
+                logger.info({ function: func, log: `update token for Org:${rotateDetails._id} ProductId:${rotateDetails.productId}` });
                 return dbQueries.updateOrgPrimaryToken(rotateDetails._id, rotateDetails.productId, rotateDetails.token, generateToken(), new Date());
-            })
-        })
+            });
+        });
 });
 
-let generateToken = function(){
-    return require('crypto').randomBytes(32).toString('hex');
+let generateToken = function() {
+    return crypto.randomBytes(32).toString('hex');
 };
 
-const getExcludedProductIds = Promise.method((dbQueries, params)=>{
+const getExcludedProductIds = Promise.method((dbQueries, _params) => {
     return dbQueries.getProducts()
-        .then((products)=>{
-            const excludedProductNames = params['keyRotation.exclusions'];
-            let excludedIds = _.map(excludedProductNames, (excludedProductName) => {
-                const product = _.find(products, (product) => {
-                    if (product.header === excludedProductName) {
-                        return product;
-                    }
+        .then((products) => {
+            const excludedProductNames = _params['keyRotation.exclusions'];
+            return _.map(excludedProductNames, (excludedProductName) => {
+                const product = _.find(products, (_product) => {
+                    return _product.header === excludedProductName;
                 });
 
                 if (!product) {
@@ -88,45 +100,47 @@ const getExcludedProductIds = Promise.method((dbQueries, params)=>{
                 }
                 return product._id;
             });
-            return excludedIds;
-        })
+        });
 });
 
 const connectDB = (services) => {
+    const func = 'handler.connectDB';
     return services.db.connect()
         .then((db) => {
-            console.log('db connected');
+            logger.info({ function: func, log: 'db connected' });
             return db;
-        })
+        });
 };
 
-const getServices = (env, region, params, services) => {
-    const username = params['defaultMongo.username'];
-    const password = params['defaultMongo.password'];
-    const replicaSet = params['defaultMongo.replicaSet'];
-    const domain = params['domain'];
+const getServices = (_env, _region, _params, _services) => {
+    const username = _params['defaultMongo.username'];
+    const password = _params['defaultMongo.password'];
+    const replicaSet = _params['defaultMongo.replicaSet'];
+    const { domain } = _params;
+    const services = _services;
 
-    services.db = new serviceImpls.DB({ env, region, domain, username, password, replicaSet, db: dbName });
+    services.db = new serviceImpls.DB({ _env, _region, domain, username, password, replicaSet, db: dbName });
 
     return services;
-}
+};
 
-const getParams = (env, region) => {
-    const keys = ['domain', 'defaultMongo.username','defaultMongo.password','defaultMongo.replicaSet','keyRotation.exclusions'];
-    const paramPrefix = `/${env}/`;
+const getParams = (_env, _region) => {
+    const func = 'handler.getParams';
+    const keys = ['domain', 'defaultMongo.username', 'defaultMongo.password', 'defaultMongo.replicaSet', 'keyRotation.exclusions'];
+    const paramPrefix = `/${_env}/`;
 
-    const params = {};
-    console.log('retrieving keys', keys, paramPrefix);
+    const tmpParams = {};
+    logger.info({ function: func, log: `retrieving keys ${keys} ${paramPrefix}` });
 
-    const loader = new ParameterStoreStaticLoader({keys, paramPrefix, env: { region } });
-    return loader.load(params)
+    const loader = new ParameterStoreStaticLoader({ keys, paramPrefix, env: { _region } });
+    return loader.load(tmpParams)
         .then(() => {
-            const retrieved = Object.keys(params).length;
-            if(!retrieved || retrieved < keys.length) {
+            const retrieved = Object.keys(tmpParams).length;
+            if (!retrieved || retrieved < keys.length) {
                 throw new Error('failed to retrieved params');
             }
-            params['keyRotation.exclusions'] = JSON.parse(params['keyRotation.exclusions']);
-            console.log(`retrieved keys - requested: ${keys.length}; retrieved: ${Object.keys(params).length}`);
-            return params;
+            tmpParams['keyRotation.exclusions'] = JSON.parse(tmpParams['keyRotation.exclusions']);
+            logger.info({ function: func, log: `retrieved keys - requested: ${keys.length}; retrieved: ${Object.keys(tmpParams).length}` });
+            return tmpParams;
         });
 };
