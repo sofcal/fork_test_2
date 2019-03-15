bc4ce15da864b629983f9a429daa8029927e6de2
Marton Blaum <marton.blaum@sage016712.sageukie.adinternal.com>
2019-03-15 15:14:38 +0000

    salt support

--- packages/module.jwks.endpoint/src/JwksCache.js
+++ packages/module.jwks.endpoint/src/JwksCache.js
@@ -2,13 +2,12 @@
 
 const BCCache = require('@sage/bc-data-cache');
 
-const mappingFunction = (data) => data;
 
 let self;
 
+
 class JwksCache {
     constructor(config, logger, parameterStore) { // TODO: rewrite me
-        const refreshFunction = this.refreshFunction;
         const endpoint = config.endpoint;
         const cacheExpiry = config.cacheExpiry;
         this.cache = new BCCache.Cache({
@@ -22,20 +21,20 @@ class JwksCache {
         this.parameterStore = parameterStore;
 
         self = this;
-
-        // this.cache.buildCache();
     }
 
     getParams() {
         return this.cache.getData();
     }
+}
 
-    refreshFunction() {  // TODO: check it
-        return self.parameterStore.getParameters([
-            `${self.paramPrefix}accessToken.primary.publicKey`,
-            `${self.paramPrefix}accessToken.secondary.publicKey`
-        ]);
-    }
+const mappingFunction = (data) => data;
+
+const refreshFunction = () => {  // TODO: check it
+    return self.parameterStore.getParameters([
+        `${self.paramPrefix}accessToken.primary.publicKey`,
+        `${self.paramPrefix}accessToken.secondary.publicKey`
+    ]);
 }
 
 module.exports = JwksCache;
--- packages/module.jwks.endpoint/test/unit/JwksEndpoint.spec.js
+++ packages/module.jwks.endpoint/test/unit/JwksEndpoint.spec.js
@@ -10,22 +10,19 @@ describe('jwks-endpoint', function() {
         throw new Error('should be stubbed')
     };
 
+    const ENV = 'local';
+    const REGION = 'eu-west-1';
+
     let sandbox;
     let config;
     let context;
     let event;
     let callback;
-    let now;
     let jwks;
     let req;
     let paramstore;
     let res = { send: errFunc};
-
-    const TEST_DATE = 1552471130970;
-    const ENV = 'local';
-    const REGION = 'eu-west-1';
-
-    const db = {connect: errFunc, disconnect: errFunc};
+    let Environment = {Environment: ENV, AWS_REGION: REGION };
 
     before(() => {
         sandbox = sinon.createSandbox();
@@ -34,7 +31,6 @@ describe('jwks-endpoint', function() {
 
     beforeEach(() => {
         context = {context: 'context', logGroupName: 'logGroupName'};
-        sandbox.stub(process, 'env').value(_.extend(process.env, {Environment: ENV, AWS_REGION: REGION }));
         event = {AWS_REGION: REGION, env: ENV};
         callback = () => {
         };
@@ -46,79 +42,170 @@ describe('jwks-endpoint', function() {
         jwks = new JwksEndpoint(config, paramstore); // TODO
 
         sandbox.stub(res);
-        now = sinon.useFakeTimers(TEST_DATE);
     });
 
     afterEach(() => {
-        now.restore();
         sandbox.restore();
     });
 
-    it('should return 500 if there is no primary key to use', () => {
-        sandbox.stub(JwksCache.prototype, 'getParams').rejects();
-        return jwks.getJwks(req, res).then(() => {
-            return Promise.all([
-                should(res.send.callCount).eql(1),
-                should(res.send.getCall(0).args[0]).eql(500),
-            ])
-        });
-    });
+    describe('jwks-endpoint without salt', function() {
 
-    it('should return 500 if there is no primary key to use', () => {
-        sandbox.stub(JwksCache.prototype, 'getParams').resolves(
-            {
+        beforeEach(() => {
+            sandbox.stub(process, 'env').value(_.extend(process.env, Environment));
+        });
+
+
+        it('should return 500 if there is no primary key to use', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').rejects();
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(500);
             });
-        return jwks.getJwks(req, res).then(() => {
-            return Promise.all([
-                should(res.send.callCount).eql(1),
-                should(res.send.getCall(0).args[0]).eql(500),
-                should(res.send.getCall(0).args[1]).eql(new Error('Failed to retrieve primary key'))
-            ])
         });
-    });
 
-    it('should return 500 if there is no secondary key to use', () => {
-        sandbox.stub(JwksCache.prototype, 'getParams').resolves(
-            {
-                '/local/accessToken.primary.publicKey': 'testPublicKey'
+        it('should return 500 if there is no primary key to use', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {});
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(500);
+                    should(res.send.getCall(0).args[1]).eql(new Error('Failed to retrieve primary key'))
             });
-        return jwks.getJwks(req, res).then(() => {
-            return Promise.all([
-                should(res.send.callCount).eql(1),
-                should(res.send.getCall(0).args[0]).eql(500),
-                should(res.send.getCall(0).args[1]).eql(new Error('Failed to retrieve secondary key'))
-            ]);
         });
-    });
 
-    it('should return 200 if there is primary and secondary key as well', () => {
-        sandbox.stub(JwksCache.prototype, 'getParams').resolves(
-            {
-                '/local/accessToken.primary.publicKey': 'testPrimaryPublicKey',
-                '/local/accessToken.secondary.publicKey': 'testSecondaryPublicKey'
+        it('should return 500 if there is no secondary key to use', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {
+                    '/local/accessToken.primary.publicKey': 'testPublicKey'
+                });
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(500);
+                    should(res.send.getCall(0).args[1]).eql(new Error('Failed to retrieve secondary key'))
             });
-        const expectedOutput = {
-            keys: [{
-                kty: 'RSA',
-                alg: 'RS256',
-                use: 'sig',
-                kid: '2b5d70f42a52651e16e2b95c374e2a49fe818b143bd3db9a55f91aceb088ed12',
-                x5c: [ 'testPrimaryPublicKey' ]
-            }, {
-                kty: 'RSA',
-                alg: 'RS256',
-                use: 'sig',
-                kid: '64d90950700000ed9a22c13f1d4b6ef8e7b37c866564b5f310d353a91be04495',
-                x5c: [ 'testSecondaryPublicKey' ]
-            }]
-        };
-        return jwks.getJwks(req, res).then(() => {
-            return Promise.all([
-                should(res.send.callCount).eql(1),
-                should(res.send.getCall(0).args[0]).eql(200),
-                should(res.send.getCall(0).args[1]).eql(expectedOutput)
-            ])
+        });
+
+        it('should return 200 if there is primary and secondary key as well', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {
+                    '/local/accessToken.primary.publicKey': 'testPrimaryPublicKey',
+                    '/local/accessToken.secondary.publicKey': 'testSecondaryPublicKey'
+                });
+            const expectedOutput = {
+                keys: [{
+                    kty: 'RSA',
+                    alg: 'RS256',
+                    use: 'sig',
+                    kid: '2b5d70f42a52651e16e2b95c374e2a49fe818b143bd3db9a55f91aceb088ed12',
+                    x5c: ['testPrimaryPublicKey']
+                }, {
+                    kty: 'RSA',
+                    alg: 'RS256',
+                    use: 'sig',
+                    kid: '64d90950700000ed9a22c13f1d4b6ef8e7b37c866564b5f310d353a91be04495',
+                    x5c: ['testSecondaryPublicKey']
+                }]
+            };
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(200);
+                    should(res.send.getCall(0).args[1]).eql(expectedOutput)
+            });
         });
     });
 
+    describe('jwks-endpoint with salt', function() {
+
+        beforeEach(() => {
+            Environment.salt = 'salt';
+            sandbox.stub(process, 'env').value(_.extend(process.env, Environment));
+        });
+
+
+        it('should return 500 if there is no primary key to use', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').rejects();
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(500);
+            });
+        });
+
+        it('should return 500 if there is no primary key to use', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {});
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(500);
+                    should(res.send.getCall(0).args[1]).eql(new Error('Failed to retrieve primary key'))
+            });
+        });
+
+        it('should return 500 if there is no secondary key to use', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {
+                    '/local/accessToken.primary.publicKey': 'testPublicKey'
+                });
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(500);
+                    should(res.send.getCall(0).args[1]).eql(new Error('Failed to retrieve secondary key'))
+            });
+        });
+
+        it('should return 200 if there is primary and secondary key as well', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {
+                    '/local/accessToken.primary.publicKey': 'testPrimaryPublicKey',
+                    '/local/accessToken.secondary.publicKey': 'testSecondaryPublicKey'
+                });
+            const expectedOutput = {
+                keys: [{
+                    kty: 'RSA',
+                    alg: 'RS256',
+                    use: 'sig',
+                    kid: '42cb21d25615431717ae051904408efc7fbcf5eb487486991c352dfd5813dcb5',
+                    x5c: ['testPrimaryPublicKey']
+                }, {
+                    kty: 'RSA',
+                    alg: 'RS256',
+                    use: 'sig',
+                    kid: '8b9b57be74954159eccd277964f7fb5b36f1d6376714f69fa218e9181f204643',
+                    x5c: ['testSecondaryPublicKey']
+                }]
+            };
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(200);
+                    should(res.send.getCall(0).args[1]).eql(expectedOutput)
+            });
+        });
+
+        it('should return 200 if there is primary and secondary key as well (with salt)', () => {
+            sandbox.stub(JwksCache.prototype, 'getParams').resolves(
+                {
+                    '/local/accessToken.primary.publicKey': 'testPrimaryPublicKey',
+                    '/local/accessToken.secondary.publicKey': 'testSecondaryPublicKey'
+                });
+            const expectedOutput = {
+                keys: [{
+                    kty: 'RSA',
+                    alg: 'RS256',
+                    use: 'sig',
+                    kid: '42cb21d25615431717ae051904408efc7fbcf5eb487486991c352dfd5813dcb5',
+                    x5c: ['testPrimaryPublicKey']
+                }, {
+                    kty: 'RSA',
+                    alg: 'RS256',
+                    use: 'sig',
+                    kid: '8b9b57be74954159eccd277964f7fb5b36f1d6376714f69fa218e9181f204643',
+                    x5c: ['testSecondaryPublicKey']
+                }]
+            };
+            return jwks.getJwks(req, res).then(() => {
+                    should(res.send.callCount).eql(1);
+                    should(res.send.getCall(0).args[0]).eql(200);
+                    should(res.send.getCall(0).args[1]).eql(expectedOutput)
+            });
+        });
+    });
 });
