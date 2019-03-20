f4d636633f4390b3f728ebfcf4fbd8f48ed14f62
Marton Blaum <marton.blaum@sage016712.sageukie.adinternal.com>
2019-03-20 16:12:24 +0000

    new format, needs refactor and expiration check

--- packages/lambda.authentication.jwks/test/int/JwksLambda.js
+++ packages/lambda.authentication.jwks/test/int/JwksLambda.js
@@ -17,27 +17,40 @@ describe('JwksLambda', function () {
 
     const paramStoreData = {
         'accessToken.primary.publicKey': 'testPrimaryPublicKey',
-        'accessToken.secondary.publicKey': 'testSecondaryPublicKey'
+        'accessToken.primary.privateKey': 'testPrimaryPrivateKey',
+        'accessToken.primary.createdAt': '1553094111',
+
+        'accessToken.secondary.publicKey': 'testSecondaryPublicKey',
+        'accessToken.secondary.privateKey': 'testSecondaryPrivateKey',
+        'accessToken.secondary.createdAt': '1553094111'
     };
 
-    const paramStorePrimary = { 'accessToken.primary.publicKey': 'testPrimaryPublicKey' }
+    const paramStorePrimary = {
+        'accessToken.primary.publicKey': 'testPrimaryPublicKey',
+        'accessToken.primary.privateKey': 'testPrimaryPrivateKey',
+        'accessToken.primary.createdAt': '1553094111'
+    };
 
-    const paramStoreSecondary = { 'accessToken.secondary.publicKey': 'testSecondaryPublicKey' };
+    const paramStoreSecondary = {
+        'accessToken.secondary.publicKey': 'testSecondaryPublicKey',
+        'accessToken.secondary.privateKey': 'testSecondaryPrivateKey',
+        'accessToken.secondary.createdAt': '1553094111'
+    };
 
     const primaryPublicKey = {
         "kty": "RSA",
         "alg": "RS256",
         "use": "sig",
-        "kid": "26bc2a4b18ad162f9c2e9f1103317fc07bf4fff0908251077e6a9154962d7ad9",
-        "x5c": "testPrimaryPublicKey"
+        "kid": "4581388218a7cc901028b172453c6922dbb4c2f6eeb5f6a9d51350731d9f21a6",
+        "x5c": ["testPrimaryPublicKey"]
     };
 
     const secondaryPublicKey = {
         "kty": "RSA",
         "alg": "RS256",
         "use": "sig",
-        "kid": "9751bce2a5126b5910ae48a94fddb5ffbb2f0bc2a1879fc84cd54eed983ec2fc",
-        "x5c": "testSecondaryPublicKey"
+        "kid": "3bcbf0ad8dc57fb595be230cbdaed770a7bdf16f22a99130bd101f40816cce50",
+        "x5c": ["testSecondaryPublicKey"]
     };
 
     const cachedData = [
@@ -89,7 +102,7 @@ describe('JwksLambda', function () {
         sandbox.stub(paramstore, 'getParameters')
             .onCall(0).resolves( paramStoreData );
         return jwksLambda.run(event, context, callback).then(() => {
-            //should(paramstore.callCount).eql(1);
+            should(paramstore.getParameters.callCount).eql(1);
 
             should(callback.callCount).eql(1);
             should(callback.getCall(0).args[0]).be.null();
@@ -106,7 +119,7 @@ describe('JwksLambda', function () {
         sandbox.stub(paramstore, 'getParameters')
             .onCall(0).resolves( paramStoreSecondary );
         return jwksLambda.run(event, context, callback).then(() => {
-           //should(Cache.prototype.getData.callCount).eql(1);
+            should(paramstore.getParameters.callCount).eql(1);
 
             should(callback.callCount).eql(1);
             should(callback.getCall(0).args[0]).be.null();
@@ -123,7 +136,7 @@ describe('JwksLambda', function () {
         sandbox.stub(paramstore, 'getParameters')
             .onCall(0).resolves( paramStorePrimary );
         return jwksLambda.run(event, context, callback).then(() => {
-            //should(Cache.prototype.getData.callCount).eql(1);
+            should(paramstore.getParameters.callCount).eql(1);
 
             should(callback.callCount).eql(1);
             should(callback.getCall(0).args[0]).be.null();
@@ -140,7 +153,7 @@ describe('JwksLambda', function () {
         sandbox.stub(paramstore, 'getParameters')
             .onCall(0).resolves( {} );
         return jwksLambda.run(event, context, callback).then(() => {
-            //should(Cache.prototype.getData.callCount).eql(1);
+            should(paramstore.getParameters.callCount).eql(1);
             should(callback.callCount).eql(1);
             should(callback.getCall(0).args[1]['statusCode']).eql(500);
 
@@ -152,7 +165,7 @@ describe('JwksLambda', function () {
         sandbox.stub(paramstore, 'getParameters')
             .onCall(0).resolves( undefined );
         return jwksLambda.run(event, context, callback).then(() => {
-            // should(Cache.prototype.getData.callCount).eql(1);
+            should(paramstore.getParameters.callCount).eql(1);
             should(callback.callCount).eql(1);
             should(callback.getCall(0).args[1]['statusCode']).eql(500);
 
@@ -164,7 +177,7 @@ describe('JwksLambda', function () {
         sandbox.stub(paramstore, 'getParameters')
             .onCall(0).rejects();
         return jwksLambda.run(event, context, callback).then(() => {
-            //should(Cache.prototype.getData.callCount).eql(1);
+            should(paramstore.getParameters.callCount).eql(1);
             should(callback.callCount).eql(1);
             should(callback.getCall(0).args[1]['statusCode']).eql(500);
 
