4036618d07b2a433be06079ba07a08b772306757
Darren Walton <darren.walton@sage.com>
2019-07-10 11:08:52 +0100

    Tidied tests.

--- packages/module.bankIds/test/int/index.spec.js
+++ packages/module.bankIds/test/int/index.spec.js
@@ -2,7 +2,7 @@ const should = require('should');
 
 describe('@sage/bc-bankids', function(){
     // placeholder
-    it('should force file to be evaluated', (done) => {
+    it('should', (done) => {
         done();
     });
 });
