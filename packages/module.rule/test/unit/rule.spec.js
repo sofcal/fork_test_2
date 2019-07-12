281fdcbab4f941cfb61ffa29b051923156e23c78
Darren Walton <darren.walton@sage.com>
2019-07-12 10:40:35 +0100

    Added rule types.

--- packages/module.rule/test/unit/rule.spec.js
+++ packages/module.rule/test/unit/rule.spec.js
@@ -5,6 +5,7 @@ const RuleSpec = require('../../lib/Rule');
 describe('@sage/bc-rule', function() {
     it('should have the correct properties', (done) => {
         RuleSpec.should.have.property('operations').which.is.a.Object();
+
         RuleSpec.operations.should.have.property('contains').which.is.a.String();
         RuleSpec.operations.should.have.property('doesNotContain').which.is.a.String();
         RuleSpec.operations.should.have.property('eq').which.is.a.String();
@@ -12,6 +13,12 @@ describe('@sage/bc-rule', function() {
         RuleSpec.operations.should.have.property('lt').which.is.a.String();
         RuleSpec.operations.should.have.property('gte').which.is.a.String();
         RuleSpec.operations.should.have.property('lte').which.is.a.String();
+
+        RuleSpec.types.should.property('user').which.is.a.String();
+        RuleSpec.types.should.property('accountant').which.is.a.String();
+        RuleSpec.types.should.property('feedback').which.is.a.String();
+        RuleSpec.types.should.property('global').which.is.a.String();
+
         Object.isFrozen(RuleSpec).should.be.true();
         done();
     });
