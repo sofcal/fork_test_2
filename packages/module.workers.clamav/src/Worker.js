f694ecac2ebc59a4ee98459cd33f3901aef0df98
wayne.smith <>
2019-07-15 01:27:00 +0100

    - fixed logging

--- packages/module.workers.clamav/src/Worker.js
+++ packages/module.workers.clamav/src/Worker.js
@@ -25,58 +25,58 @@ class Worker {
 
     start() {
         const func = `${Worker.name}.start`;
-        this.logger.info({ function: func, msg: 'started', params: { worker_name: this.name } });
+        this.logger.info({ function: func, msg: 'started', params: { workerName: this.name } });
         this.active = true;
 
         poll(this);
-        this.logger.info({ function: func, msg: 'ended', params: { worker_name: this.name } });
+        this.logger.info({ function: func, msg: 'ended', params: { workerName: this.name } });
     }
 
     stop() {
         const func = `${Worker.name}.stop`;
-        this.logger.info({ function: func, msg: 'started', params: { worker_name: this.name } });
+        this.logger.info({ function: func, msg: 'started', params: { workerName: this.name } });
         this.active = false;
 
-        this.logger.info({ function: func, msg: 'ended', params: { worker_name: this.name } });
+        this.logger.info({ function: func, msg: 'ended', params: { workerName: this.name } });
     }
 
     waitOnTask() {
         const func = `${Worker.name}.waitOnTask`;
-        this.logger.debug({ function: func, msg: 'started', params: { worker_name: this.name } });
+        this.logger.debug({ function: func, msg: 'started', params: { workerName: this.name } });
         return Promise.resolve(undefined)
             .then(() => {
                 const taskOptions = { activityArn: this.activityArn, workerName: this.name };
                 return this.services.step.getActivityTask(taskOptions).promise()
                     .then(({ taskToken, input = '{}' }) => {
                         if (!taskToken) {
-                            this.logger.debug({ function: func, msg: 'no task after long poll. Retrying', params: { worker_name: this.name } });
+                            this.logger.debug({ function: func, msg: 'no task after long poll. Retrying', params: { workerName: this.name } });
                             return undefined;
                         }
 
-                        this.logger.info({ function: func, msg: 'retrieved task', params: { worker_name: this.name, taskToken } });
+                        this.logger.info({ function: func, msg: 'retrieved task', params: { workerName: this.name, taskToken } });
                         const parsed = JSON.parse(input);
 
-                        return scan(this.services, taskToken, parsed, { logger: this.logger })
+                        return scan(this.services, taskToken, parsed, { logger: this.logger, workerName: this.name })
                             .then((result) => {
                                 parsed.scanStatus = result.is_infected ? consts.INFECTED : consts.CLEAN;
                                 const successOptions = { taskToken, output: JSON.stringify(parsed) };
 
-                                this.logger.info({ function: func, msg: 'sending success result', params: { worker_name: this.name, is_infected: result.is_infected, scanStatus: successOptions.scanStatus } });
+                                this.logger.info({ function: func, msg: 'sending success result', params: { workerName: this.name, is_infected: result.is_infected, scanStatus: successOptions.scanStatus } });
                                 return this.services.step.sendTaskSuccess(successOptions).promise();
                             })
                             .catch((err) => {
-                                this.logger.error({ function: func, msg: 'error occurred during scan. Sending failure result', params: { worker_name: this.name, error: err.message } });
+                                this.logger.error({ function: func, msg: 'error occurred during scan. Sending failure result', params: { workerName: this.name, error: err.message } });
                                 const failureOptions = { taskToken, cause: 'AV Check error', error: err.message };
                                 return this.services.step.sendTaskFailure(failureOptions).promise();
                             });
                     });
             })
             .catch((err) => {
-                this.logger.error({ function: func, msg: 'error attempting to retrieve task. Swallowing', params: { worker_name: this.name, error: err.message } });
+                this.logger.error({ function: func, msg: 'error attempting to retrieve task. Swallowing', params: { workerName: this.name, error: err.message } });
             })
             .then(() => Promise.delay(1000))
             .then(() => {
-                this.logger.debug({ function: func, msg: 'ended', params: { worker_name: this.name } });
+                this.logger.debug({ function: func, msg: 'ended', params: { workerName: this.name } });
             });
     }
 }
@@ -90,32 +90,32 @@ const poll = Promise.method((self) => {
         });
 });
 
-const scan = Promise.method((services, taskToken, parsed, { logger }) => {
+const scan = Promise.method((services, taskToken, parsed, { logger, workerName }) => {
     const func = `${Worker.name}.scan`;
-    logger.info({ function: func, msg: 'started', params: { worker_name: this.name } });
+    logger.info({ function: func, msg: 'started', params: { workerName } });
 
     const bucket = parsed.bucket;
     const key = `${parsed.prefix}${parsed.fileName}`;
 
-    logger.info({ function: func, msg: 'retrieving file from s3', params: { worker_name: this.name, bucket, key } });
+    logger.info({ function: func, msg: 'retrieving file from s3', params: { workerName, bucket, key } });
 
     return services.s3.get(key, bucket)
         .catch((err) => {
-            logger.info({ function: func, msg: 'error retrieving file from s3', params: { worker_name: this.name, error: err.message } });
+            logger.info({ function: func, msg: 'error retrieving file from s3', params: { workerName, error: err.message } });
             throw err;
         })
         .then((data) => {
-            logger.info({ function: func, msg: 'retrieved file from s3', params: { worker_name: this.name } });
+            logger.info({ function: func, msg: 'retrieved file from s3', params: { workerName } });
             if (!data.Body) {
-                logger.info({ function: func, msg: 'file had no content', params: { worker_name: this.name } });
+                logger.info({ function: func, msg: 'file had no content', params: { workerName } });
                 throw new Error('s3 file is empty');
             }
 
-            logger.info({ function: func, msg: 'beginning scan', params: { worker_name: this.name } });
+            logger.info({ function: func, msg: 'beginning scan', params: { workerName } });
             const stream = bufferToStream(data.Body);
             return services.clam.scan_stream(stream)
                 .then((result) => {
-                    logger.info({ function: func, msg: 'ended', params: { worker_name: this.name } });
+                    logger.info({ function: func, msg: 'ended', params: { workerName } });
                     return result;
                 });
         });
