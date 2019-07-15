4523431d30217d6a64a44af17266047dffc24843
tom.wyatt <tom.wyatt@sage.com>
2019-07-15 10:41:55 +0100

    Pushing to create the query module so that uncappedquery can be written properly.

---
+++ packages/module.query/test/unit/Query.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Query = require('../../lib/Query')();
+
+describe('@sage/bc-query', function() {
+    it('should export a function which is an instance of Query and has the correct properties', (done) => {
+        Query.should.be.a.Function();
+        Query.should.have.property('startIndex').which.is.a.Number();
+        Query.should.have.property('count').which.is.a.Number();
+        Query.should.have.property('where').which.is.an.Object();
+        Query.should.have.property('orderBy');
+        Query.should.have.property('groupBy');
+        Query.should.have.property('projection');
+
+        done();
+    });
+});
---
+++ packages/module.uncappedquery/test/unit/uncappedQuery.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+var maxCount = 50;
+
+function Query(startIndex, count, where) {
+
+    this.startIndex = parseInt(startIndex, 10);
+    this.count = parseInt(count, 10);
+    this.where = where ? where : {};
+    this.orderBy = undefined;
+    this.groupBy = undefined;
+    this.projection = undefined;
+
+    this.validate();
+}
+var p = Query.prototype;
+
+p.validate = function(){
+
+    if (this.startIndex < 0 || this.startIndex === undefined || isNaN(this.startIndex)){
+        this.startIndex = 0;
+    }
+
+    if (this.count <= 0 || this.count > maxCount || this.count === undefined || isNaN(this.count)){
+        this.count = maxCount;
+    }
+};
+
+const query = new Query;
+const UncappedQuery = require('../../lib/UncappedQuery')(1, 2, { 'someId': '012345678' }, query);
+
+describe('@sage/bc-uncappedquery', function() {
+    it('should export a function which is an instance of Query', (done) => {
+        console.log('>>>>>>>>>>> ' + JSON.stringify(UncappedQuery));
+        console.log('------- ' + JSON.stringify(UncappedQuery.prototype));
+        UncappedQuery.should.be.a.Function();
+        (UncappedQuery.prototype).should.be.an.instanceOf(Query);
+
+        done();
+    });
+});
