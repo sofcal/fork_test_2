257a77ac1adb001b8bb90ac684d075b1b185e781
wayne.smith <>
2019-07-15 12:08:22 +0100

    WPBBC-210
    Squashed commit of the following:
    
    commit 4574785d3e96c78e00c67852979d8e49aaceb573
    Author: wayne.smith <>
    Date:   Mon Jul 15 03:38:21 2019 +0100
    
        - package lock
    
    commit 3a4ce8c5c93689e468d0bb0a1f224f5b117e4610
    Author: wayne.smith <>
    Date:   Mon Jul 15 01:27:08 2019 +0100
    
        v1.1.35
    
    commit f694ecac2ebc59a4ee98459cd33f3901aef0df98
    Author: wayne.smith <>
    Date:   Mon Jul 15 01:27:00 2019 +0100
    
        - fixed logging
    
    commit 64d6e69bd6c6fa6be5b14874de44b369a4465377
    Author: wayne.smith <>
    Date:   Sun Jul 14 23:43:30 2019 +0100
    
        - lock update
    
    commit ff76503a0bc7cfe8b87fab3aca2f5c4c1347cd54
    Author: wayne.smith <>
    Date:   Sun Jul 14 23:31:51 2019 +0100
    
        v1.1.34
    
    commit 0f7762f2af130459ed95cdded6384af97286af2d
    Author: wayne.smith <>
    Date:   Sun Jul 14 23:31:32 2019 +0100
    
        - updates to allow proper logging
        - updates to some output
    
    commit 1e058fef19e92467a9d44ec97ec770daa455829e
    Author: wayne.smith <>
    Date:   Sun Jul 14 18:14:04 2019 +0100
    
        v1.1.33
    
    commit 5e65a1165420ab476be9ca4c99881173fbc92cec
    Author: wayne.smith <>
    Date:   Sun Jul 14 18:13:37 2019 +0100
    
        - removed bc legacy wrapper, in favour of the original jwt one
        - updated workers.clamav to export correctly
        - added workers.clamav to jwt legacywrapper
    
    commit cb38192292650a87a4f226abfce41588deb9545f
    Author: wayne.smith <>
    Date:   Sun Jul 14 14:43:39 2019 +0100
    
        - adding package-lock
    
    commit d043bd0148ba4c05939d140a2d1f0ec7d6caacdd
    Author: wayne.smith <>
    Date:   Sun Jul 14 14:26:09 2019 +0100
    
        v1.1.32
    
    commit 29713939866b3c259bc43480dd75704422f2521e
    Author: wayne.smith <>
    Date:   Sun Jul 14 14:05:19 2019 +0100
    
        v1.1.31
    
    commit f55522a92ef58df86cd15a2127e04d40c6013ba6
    Author: wayne.smith <>
    Date:   Sun Jul 14 14:04:42 2019 +0100
    
        - legacy wrapper for BC modules
    
    commit 570d83c83127233ba0852b2ffb6eb68485dc83b1
    Author: wayne.smith <>
    Date:   Sun Jul 14 14:04:04 2019 +0100
    
        - updates to allow versioning and publish
    
    commit bee001732f23da017adf288e9c9279af53e4496c
    Author: wayne.smith <>
    Date:   Sun Jul 14 13:48:27 2019 +0100
    
        - build 1.1.30
    
    commit ae0892fd87f053db0cffcab89f96a28369503419
    Author: wayne.smith <>
    Date:   Sun Jul 14 13:46:59 2019 +0100
    
        v1.1.30
    
    commit 4efde57ecf052e02bf1a81be1dec962e8e35fd32
    Author: wayne.smith <>
    Date:   Sun Jul 14 13:42:44 2019 +0100
    
        v1.1.20
    
    commit 2094068f0da2def2d8586fa98f5811f9eead93f0
    Author: wayne.smith <>
    Date:   Sun Jul 14 13:42:17 2019 +0100
    
        v1.1.13
    
    commit 076c05cecbba183bb11f0a7410f1c386f00d558d
    Author: wayne.smith <>
    Date:   Sun Jul 14 13:41:35 2019 +0100
    
        v1.1.12
    
    commit 531f5d153865c5df9bc42faa5154911b9212381d
    Author: wayne.smith <>
    Date:   Sun Jul 14 13:40:54 2019 +0100
    
        - first pass at worker using clamd to scan streams
    
    commit 1d011340dc504b685e7c1a3d2cde033c5f0f7f92
    Author: wayne.smith <>
    Date:   Sat Jul 13 20:13:48 2019 +0100
    
        - worker module for polling AWS activity tasks and performing AV scan
    
    commit 7895c5986872cd023a19a45515a3f45612e7a709
    Author: wayne.smith <>
    Date:   Sat Jul 13 19:11:33 2019 +0100
    
        - updates to build process to use new babel config and transpile async/await
    
    commit e7cc8bcf73b47b38c5d965dea7955eb2839f1d91
    Author: wayne.smith <>
    Date:   Sat Jul 13 16:08:13 2019 +0100
    
        - initial push of clamscan fork so I can track changes in removing asnyc/await

---
+++ packages/module.workers.clamav/src/Worker.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const { Readable } = require('stream');
+
+const Promise = require('bluebird');
+
+const consts = {
+    INFECTED: 'Infected',
+    CLEAN: 'Clean'
+};
+
+class Worker {
+    constructor({ step, s3, activityArn, clam, name }, { logger }) {
+        this.logger = logger;
+
+        this.activityArn = activityArn;
+        this.name = name;
+        this.services = { step, s3, clam };
+        this.active = false;
+    }
+
+    static Create(...args) {
+        return new Worker(...args);
+    }
+
+    start() {
+        const func = `${Worker.name}.start`;
+        this.logger.info({ function: func, msg: 'started', params: { workerName: this.name } });
+        this.active = true;
+
+        poll(this);
+        this.logger.info({ function: func, msg: 'ended', params: { workerName: this.name } });
+    }
+
+    stop() {
+        const func = `${Worker.name}.stop`;
+        this.logger.info({ function: func, msg: 'started', params: { workerName: this.name } });
+        this.active = false;
+
+        this.logger.info({ function: func, msg: 'ended', params: { workerName: this.name } });
+    }
+
+    waitOnTask() {
+        const func = `${Worker.name}.waitOnTask`;
+        this.logger.debug({ function: func, msg: 'started', params: { workerName: this.name } });
+        return Promise.resolve(undefined)
+            .then(() => {
+                const taskOptions = { activityArn: this.activityArn, workerName: this.name };
+                return this.services.step.getActivityTask(taskOptions).promise()
+                    .then(({ taskToken, input = '{}' }) => {
+                        if (!taskToken) {
+                            this.logger.debug({ function: func, msg: 'no task after long poll. Retrying', params: { workerName: this.name } });
+                            return undefined;
+                        }
+
+                        this.logger.info({ function: func, msg: 'retrieved task', params: { workerName: this.name, taskToken } });
+                        const parsed = JSON.parse(input);
+
+                        return scan(this.services, taskToken, parsed, { logger: this.logger, workerName: this.name })
+                            .then((result) => {
+                                parsed.scanStatus = result.is_infected ? consts.INFECTED : consts.CLEAN;
+                                const successOptions = { taskToken, output: JSON.stringify(parsed) };
+
+                                this.logger.info({ function: func, msg: 'sending success result', params: { workerName: this.name, is_infected: result.is_infected, scanStatus: successOptions.scanStatus } });
+                                return this.services.step.sendTaskSuccess(successOptions).promise();
+                            })
+                            .catch((err) => {
+                                this.logger.error({ function: func, msg: 'error occurred during scan. Sending failure result', params: { workerName: this.name, error: err.message } });
+                                const failureOptions = { taskToken, cause: 'AV Check error', error: err.message };
+                                return this.services.step.sendTaskFailure(failureOptions).promise();
+                            });
+                    });
+            })
+            .catch((err) => {
+                this.logger.error({ function: func, msg: 'error attempting to retrieve task. Swallowing', params: { workerName: this.name, error: err.message } });
+            })
+            .then(() => Promise.delay(1000))
+            .then(() => {
+                this.logger.debug({ function: func, msg: 'ended', params: { workerName: this.name } });
+            });
+    }
+}
+
+const poll = Promise.method((self) => {
+    return self.waitOnTask()
+        .then(() => {
+            if (self.active) {
+                poll(self);
+            }
+        });
+});
+
+const scan = Promise.method((services, taskToken, parsed, { logger, workerName }) => {
+    const func = `${Worker.name}.scan`;
+    logger.info({ function: func, msg: 'started', params: { workerName } });
+
+    const bucket = parsed.bucket;
+    const key = `${parsed.prefix}${parsed.fileName}`;
+
+    logger.info({ function: func, msg: 'retrieving file from s3', params: { workerName, bucket, key } });
+
+    return services.s3.get(key, bucket)
+        .catch((err) => {
+            logger.info({ function: func, msg: 'error retrieving file from s3', params: { workerName, error: err.message } });
+            throw err;
+        })
+        .then((data) => {
+            logger.info({ function: func, msg: 'retrieved file from s3', params: { workerName } });
+            if (!data.Body) {
+                logger.info({ function: func, msg: 'file had no content', params: { workerName } });
+                throw new Error('s3 file is empty');
+            }
+
+            logger.info({ function: func, msg: 'beginning scan', params: { workerName } });
+            const stream = bufferToStream(data.Body);
+            return services.clam.scan_stream(stream)
+                .then((result) => {
+                    logger.info({ function: func, msg: 'ended', params: { workerName } });
+                    return result;
+                });
+        });
+});
+
+const bufferToStream = (data) => {
+    const readable = new Readable();
+    readable._read = () => {}; // _read should be used to populate the stream, but we're pushing direct to it as we have the data in memory
+    readable.push(data);
+    readable.push(null);
+    readable.destroy = () => {
+        // pass-through to allow this stream to work with clamscan
+    };
+
+    return readable;
+};
+
+module.exports = Worker;
