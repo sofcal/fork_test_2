7c3862a1c7a839c772a9996a43ee1741064de334
tom.wyatt <tom.wyatt@sage.com>
2019-07-16 10:41:52 +0100

    Updates to uncappedquery

---
+++ packages/module.uncappedquery/src/Query.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const maxCount = 50;
+
+function Query(startIndex, count, where){
+
+    this.startIndex = parseInt(startIndex, 10);
+    this.count = parseInt(count, 10);
+    this.where = where ? where : {};
+    this.orderBy = undefined;
+    this.groupBy = undefined;
+    this.projection = undefined;
+
+    this.validate();
+}
+let p = Query.prototype;
+
+p.validate = function(){
+
+    if (this.startIndex < 0 || this.startIndex === undefined || isNaN(this.startIndex)){
+        this.startIndex = 0;
+    }
+
+    if (this.count <= 0 || this.count > maxCount || this.count === undefined || isNaN(this.count)){
+        this.count = maxCount;
+    }
+};
+
+module.exports = Query;
