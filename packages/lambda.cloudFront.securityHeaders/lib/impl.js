cefd6cdb084adcd8d26879ea4f289c751042d8b2
graham.postles <graham.postles@sage.com>
2019-07-24 16:18:01 +0100

    Squashed commit of the following:
    commit 3e5fc5902dd1b56da3463043d30f77702e0388b1
    Author: graham.postles <graham.postles@sage.com>
    Date:   Wed Jul 24 16:07:43 2019 +0100
    
        Updated dependencies
    
    commit f67b85e60372f6815d82bcb80dc768a9d63ee763
    Merge: 4e50a31 858955b
    Author: graham.postles <graham.postles@sage.com>
    Date:   Wed Jul 24 14:19:44 2019 +0100
    
        Merge branch 'develop' of https://github.com/Sage/wpb-banking-support into feature/securityHeaders
    
        # Conflicts:
        #	packages/module.forks.clamscan/package-lock.json
    
    commit 4e50a31e69eec1830a85fd4d43e44ed5f37d03b1
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Apr 2 13:45:06 2019 +0100
    
        remove promisifyAll, remove unrequired stubs in tests, set Environment on headers
    
    commit 7b6fd5cd5c1c85c7b2198043f12d67a37b63ecf5
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Apr 2 11:55:39 2019 +0100
    
        remove .only
    
    commit 102f9ed9a34a846e0a3236b82c91022f4c8c46f2
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Apr 2 11:41:46 2019 +0100
    
        update service name
    
    commit 873ab885a3f54014c84a8593b3a19e690ee02ce3
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Apr 2 10:55:20 2019 +0100
    
        Import into GitHub and rename
    
    commit b1a208569af7159ab7b37f2384637a0b87a09e87
    Merge: fb8c6c1 1f2e499
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Apr 2 10:27:29 2019 +0100
    
        Merge branch 'feature/edgelambda' of https://github.com/Sage/wpb-banking-support into feature/securityHeaders
    
    commit 1f2e49953a801285b688e94f1ecb7322f1f6e89b
    Author: graham.postles <graham.postles@sage.com>
    Date:   Fri Mar 29 15:23:34 2019 +0000
    
        use downloaded @sage packages, not internal
    
    commit 76caa95daa67a95c016af02c699c99b7af7e95e5
    Author: graham.postles <graham.postles@sage.com>
    Date:   Thu Mar 7 16:54:13 2019 +0000
    
        X frame domain and get env from custom headers
    
    commit 478a4ac2b7ab3b9bcf2bdbafd2b9ea291c8de6a9
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Feb 19 13:12:43 2019 +0000
    
        tidy up units prior to wildcard change
    
    commit c79417c029058c6a3ee6b95ae5f87ae83fe9fc60
    Author: graham.postles <graham.postles@sage.com>
    Date:   Tue Feb 19 12:02:31 2019 +0000
    
        update units so thay work with Marks requested changes
    
    commit 2b36ccaa6330c5427766c8f6b6b426cbb191f90c
    Author: graham.postles <graham.postles@sage.com>
    Date:   Mon Feb 18 08:44:19 2019 +0000
    
        Added x-contebt-security-policy for captcha
    
    commit ec5bb55f53abbb9a9a5d8201a683ab93294d0f75
    Author: graham.postles <graham.postles@sage.com>
    Date:   Thu Jan 24 15:53:03 2019 +0000
    
        Add new security headers from Richard and fix bug in xframedomain check
    
    commit a24efe04ee8c6793e86575b3e45b45117461d36d
    Author: graham.postles <graham.postles@sage.com>
    Date:   Wed Jan 16 17:10:58 2019 +0000
    
        Add cloudfront security headers

---
+++ packages/lambda.cloudFront.securityHeaders/lib/impl.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const Promise = require('bluebird');
+const ErrorSpecs = require('./ErrorSpecs');
+const { StatusCodeError } = require('@sage/bc-status-code-error');
+const querystring = require('querystring');
+const _ = require('underscore');
+
+module.exports.run = Promise.method((event, params) => {
+    const func = 'impl.run';
+    event.logger.info({ function: func, log: 'started' });
+
+    const response = event.Records[0].cf.response; // eslint-disable-line prefer-destructuring
+    const request = event.Records[0].cf.request; // eslint-disable-line prefer-destructuring
+    const headers = response.headers; // eslint-disable-line prefer-destructuring
+    const query = request.querystring.toLowerCase();
+    const uri = request.uri.toLowerCase();
+    event.logger.info({ function: func, log: 'request', params: { request } });
+
+    try {
+        const whitelist = params['outboundApi.callbackUriWhiteList'] ? params['outboundApi.callbackUriWhiteList'] : '';
+        const whitelistNoCommas = whitelist.toString().split(',').join(' ');
+
+        // remove unwanted headers
+        // Set to empty string because if I remove the default s3 server, cloudfront then adds 'CloudFront'
+        headers['server'] = [{ key: 'Server', value: '' }]; // eslint-disable-line dot-notation
+
+        // Add static security headers
+        event.logger.info({ function: func, log: 'Adding static security headers' });
+        headers['cache-control'] = [{ key: 'Cache-Control', value: 'public, max-age=86400, no-transform' }];
+        headers['strict-transport-security'] = [{ key: 'Strict-Transport-Security', value: 'max-age=15552000; includeSubDomains' }];
+        headers['referrer-policy'] = [{ key: 'Referrer-Policy', value: 'same-origin' }];
+        headers['feature-policy'] = [{ key: 'Feature-Policy', value: 'geolocation \'none\'; midi \'none\'; sync-xhr \'self\'; microphone \'none\'; camera \'none\'; magnetometer \'none\'; gyroscope \'none\'; speaker \'none\'; fullscreen \'self\'; payment \'self\'' }];
+
+        headers['x-xss-protection'] = [{ key: 'X-XSS-Protection', value: '1; mode=block' }];
+        headers['x-content-type-options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
+        headers['x-download-options'] = [{ key: 'X-Download-Options', value: 'noopen' }];
+        headers['x-permitted-cross-domain-policies'] = [{ key: 'X-Permitted-Cross-Domain-Policies', value: 'master-only' }];
+
+        if (whitelist && whitelist !== '') {
+            headers['x-content-security-policy'] = [{ key: 'X-Content-Security-Policy', value: `allow-from frame-ancestors ${whitelistNoCommas}` }];
+            let headerValue = `img-src 'self' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://s3-eu-west-1.amazonaws.com ${whitelistNoCommas}; `;
+            headerValue += `script-src 'self' *.sage.com 'unsafe-inline' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://cdn.plaid.com ${whitelistNoCommas}; `;
+            headerValue += `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com ${whitelistNoCommas}; `;
+            headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: headerValue }];
+        } else {
+            headers['x-content-security-policy'] = [{ key: 'X-Content-Security-Policy', value: 'default-src \'none\'' }];
+            let headerValue = 'img-src \'self\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://s3-eu-west-1.amazonaws.com; ';
+            headerValue += 'script-src \'self\' \'unsafe-inline\' https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://cdn.plaid.com; ';
+            headerValue += 'style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com https://fonts.gstatic.com; ';
+            headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: headerValue }];
+        }
+        event.logger.info({ function: func, log: 'added x-frame-options allow-from for domain', params: { xFrameOptions: headers['x-content-security-policy'][0].value } });
+
+        event.logger.info({ function: func, log: 'query', params: { query } });
+        const xFrameDomain = getQuerySegment(query, 'xframedomain');
+        event.logger.info({ function: func, log: 'xFrameDomain', params: { xFrameDomain } });
+        if (checkXFrameDomainInWhiteList(xFrameDomain, whitelist)) {
+            // if xFrameDomain in query string and in whitelist then add allow-from
+            headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: `ALLOW-FROM ${xFrameDomain}` }];
+            event.logger.info({ function: func, log: 'added ', params: { xFrameOptions: headers['x-frame-options'][0].value } });
+        } else if (uri.includes('xframe.html')) {
+            // if xFrameDomain NOT in query and page is xframe.html then add DENY
+            headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
+            event.logger.info({ function: func, log: 'added ', params: { xFrameOptions: headers['x-frame-options'][0].value } });
+        }
+    } catch (e) {
+        event.logger.error({ function: func, log: 'error adding headers', params: { errorMessage: e.message } });
+        throw StatusCodeError.CreateFromSpecs([ErrorSpecs.generalError], ErrorSpecs.generalError.statusCode);
+    }
+    event.logger.info({ function: func, log: 'ended' });
+    return response;
+});
+
+const checkXFrameDomainInWhiteList = (xFrameDomain, whiteList) => {
+    let exists = false;
+    if (xFrameDomain) {
+        const whiteListArray = whiteList.toString().split(',');
+        let lowDomain = '';
+        const lowXFrameDomain = xFrameDomain.toString().toLowerCase();
+        _.find(whiteListArray, (domain) => {
+            lowDomain = domain.toString().toLowerCase();
+            const pos = lowDomain.indexOf('*.');
+            if (pos > -1) {
+                lowDomain = lowDomain.substring(pos + 2);
+                if (lowXFrameDomain.endsWith(lowDomain)) {
+                    exists = true;
+                }
+            } else if (lowXFrameDomain.endsWith(lowDomain)) {
+                exists = true;
+            }
+        });
+        if (lowXFrameDomain.includes(lowDomain)) {
+            exists = true;
+        }
+    }
+    return exists;
+};
+
+const getQuerySegment = (query, segment) => {
+    if (query.length < 1) {
+        return undefined;
+    }
+    const queryObj = querystring.parse(query);
+    return queryObj[segment];
+};
