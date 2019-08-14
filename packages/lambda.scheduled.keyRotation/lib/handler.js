6b69f51d160a856ffb49b25352ec95b999c5d2ef
Gray <graham.seymore@sage.com>
2019-08-14 17:37:24 +0100

    - ParameterStoreStaticLoader needs to be destructed - pass in region rather than _region

--- packages/lambda.scheduled.keyRotation/lib/handler.js
+++ packages/lambda.scheduled.keyRotation/lib/handler.js
@@ -3,7 +3,7 @@
 const Promise = require('bluebird');
 const DbQueries = require('./dbQueries');
 const _ = require('underscore');
-const ParameterStoreStaticLoader = require('@sage/bc-parameterstore-static-loader');
+const { ParameterStoreStaticLoader } = require('@sage/bc-parameterstore-static-loader');
 const BcServicesDb = require('@sage/bc-services-db');
 const crypto = require('crypto');
 const { RequestLogger } = require('@sage/bc-requestlogger');
@@ -124,23 +124,23 @@ const getServices = (_env, _region, _params, _services) => {
     return services;
 };
 
-const getParams = (_env, _region) => {
+const getParams = (env, region) => {
     const func = 'handler.getParams';
     const keys = ['domain', 'defaultMongo.username', 'defaultMongo.password', 'defaultMongo.replicaSet', 'keyRotation.exclusions'];
-    const paramPrefix = `/${_env}/`;
+    const paramPrefix = `/${env}/`;
 
-    const tmpParams = {};
+    const params = {};
     logger.info({ function: func, log: `retrieving keys ${keys} ${paramPrefix}` });
 
-    const loader = new ParameterStoreStaticLoader({ keys, paramPrefix, env: { _region } });
-    return loader.load(tmpParams)
+    const loader = new ParameterStoreStaticLoader({ keys, paramPrefix, env: { region } });
+    return loader.load(params)
         .then(() => {
-            const retrieved = Object.keys(tmpParams).length;
+            const retrieved = Object.keys(params).length;
             if (!retrieved || retrieved < keys.length) {
                 throw new Error('failed to retrieved params');
             }
-            tmpParams['keyRotation.exclusions'] = JSON.parse(tmpParams['keyRotation.exclusions']);
-            logger.info({ function: func, log: `retrieved keys - requested: ${keys.length}; retrieved: ${Object.keys(tmpParams).length}` });
-            return tmpParams;
+            params['keyRotation.exclusions'] = JSON.parse(params['keyRotation.exclusions']);
+            logger.info({ function: func, log: `retrieved keys - requested: ${keys.length}; retrieved: ${Object.keys(params).length}` });
+            return params;
         });
 };
