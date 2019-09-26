fc6815e53364a22d004dcd823470486b1ee024cb
Tom Wyatt <tom.wyatt@sage.com>
2019-09-26 08:15:16 +0100

    Update to bank. Fixing issue in int test.

--- packages/module.bank/test/int/index.spec.js
+++ packages/module.bank/test/int/index.spec.js
@@ -78,7 +78,6 @@ describe('@sage/bc-bank.Bank', () => {
         ];
         status = 'supported';
         internalStatuses = [Bank.internalStatusValues.clientDataFlowBlocked];
-        internal = Bank.
         address = '1 Bank Street, Financial District, Bank Town, BA11 NK11';
         emailAddress = 'hello@usbank.com';
         agreement = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
