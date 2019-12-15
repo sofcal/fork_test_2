abdc91fe2c732558df364ed119a0dd85dbb86552
wayne.smith <>
2019-12-15 19:45:57 +0000

    - lambda service

---
+++ packages/module.services.lambda/test/unit/index.spec.js
@@ -0,0 +1,12 @@
+const Lambda = require('../../src/index');
+const should = require('should');
+
+describe.only('@sage/bc-services-lambda.index', function(){
+    this.timeout(10000);
+    // placeholder
+    it('should force file to be evaluated', () => {
+        // const lambda = new Lambda({ region: 'eu-west-1' });
+        //
+        // return lambda.invoke({ functionName: 'papi-dev02-eu-west-1-BatchStatus', payload: '' })
+    });
+});
