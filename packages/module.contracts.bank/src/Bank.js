81e0c9eb18cfee756b3cb55da176ee6a7a9a0d12
Jamie Weston <jamie.weston@sage.com>
2020-03-13 11:40:16 +0000

    Wpbbc 2214 ts and cs (#67)
    * Moving bankURL to top level and making casing more consistent.
    Updated tests to reflect new location of field.
    
    * v1.1.135
    
    * v1.1.136
    
    * Added new value to enums for Bank.authorisationMechanism
    
    * v1.1.137
    
    * v1.1.138
    
    * Added CloudID Authenticator to jwt wrapper
    
    * v1.1.139
    
    * Added Bank.bankURL to validation and tests
    
    * v1.1.140
    
    * test case description update.
    
    Co-authored-by: Tom Wyatt <tom.wyatt@sage.com>

--- packages/module.contracts.bank/src/Bank.js
+++ packages/module.contracts.bank/src/Bank.js
@@ -38,13 +38,13 @@ class Bank {
 
             this.internal = {
                 longURL: access(data, 'internal.longURL') || false,
-                bankUrl: access(data, 'internal.bankUrl') || null,
                 minRequestedStartDate: access(data, 'internal.minRequestedStartDate') || null
             };
 
             this.provider = {};
             this.provider.authUrl = access(data, 'provider.authUrl') || null;
             this.provider.providerId = access(data, 'provider.providerId') || null;
+            this.bankURL = access(data, 'bankURL') || null;
 
             /* aggregator feature */
             if (plaidFeatureEnabled) {
@@ -96,7 +96,8 @@ class Bank {
             { path: 'recentFileHistory', nested: val.validateRecentFileHistory, optional: true, allowNull: true },
             { path: 'internal', nested: val.validateInternal, optional: true, allowNull: true },
             { path: 'provider', nested: val.validateProvider, optional: false, allowNull: false },
-            { path: 'popularBank', custom: _.isBoolean, optional: false, allowNull: false }
+            { path: 'popularBank', custom: _.isBoolean, optional: false, allowNull: false },
+            { path: 'bankURL', custom: _.isString, optional: true, allowNull: true }
         ];
 
         /* aggregator feature */
