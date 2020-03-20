4e1448972faa40509839967a337330b15c515ebb
Jamie Weston <monotone2k@gmail.com>
2020-03-20 09:38:32 +0000

    Fleshed out AppConfigLoader

--- packages/module.framework.parametercache/src/loaders/AppConfigLoader.js
+++ packages/module.framework.parametercache/src/loaders/AppConfigLoader.js
@@ -1,20 +1,76 @@
 'use strict';
 
 const Promise = require('bluebird');
+const AWS = require('aws-sdk');
+const uuidGen = require('uuid');
+
+const jsonMime = 'application/json';
 
 class AppConfigLoader {
-    constructor({ envPrefix = null, env = null }) {
-        console.log('>>>> [AppConfigLoader] created');
-        this.envPrefix = envPrefix;
-        this.env = env;
+    constructor({ appConfigApplication, paramPrefix, env: { region } = {}, appConfig }) {
+        if (!region) {
+            throw new Error('[AppConfigLoader] invalid region');
+        }
+
+        if (!paramPrefix) {
+            throw new Error('[AppConfigLoader] invalid paramPrefix');
+        }
+
+        this.clientId = uuidGen.v4();
+
+        this.appConfig = appConfig || Promise.promisifyAll(new AWS.AppConfig({ apiVersion: '2019-10-09' }));
+
+        this.application = appConfigApplication;
+        this.environment = paramPrefix;
+        this.region = region;
+
+        this.configuration = {};
     }
 
     load(params) {
-        console.log('>>>> [AppConfigLoader] load() called');
         return loadImpl(this, params);
     }
 }
 
-const loadImpl = Promise.method((self, params) => {});
+const loadImpl = async(self, params) => {
+    const {
+        application,
+        environment,
+        region,
+        clientId
+    } = self;
+
+    // figure out which configuration file we need (based on application, environment and region)
+    const req = {
+        Application: application,
+        ClientId: clientId,
+        Configuration: `${application}-${environment}-${region}`,
+        Environment: `${environment}-${region}`
+    };
+
+    let content, contentType;
+
+    try {
+        ({ Content: content, ContentType: contentType } = await self.appConfig.getConfigurationAsync(req));
+
+        if (contentType !== jsonMime) {
+            console.error(`[AppConfigLoader] Invalid content type returned by AWS AppConfig (expected ${jsonMime}, received ${ContentType}, returning last known good configuration.`);
+        } else {
+            self.configuration = content;
+        }
+    } catch {
+        console.error(`[AppConfigLoader] Failed to retrieve configuration file from AppConfig, returning last known good configuration.`);
+    }
+
+    return mapResponse(params, self.configuration);
+};
+
+// We always expect a flat file, similar to the response from SSM Parameter Store, but we
+// don't have to worry about prefixes since each environment has its own configuration file.
+const mapResponse = (params, response) => {
+    for (let [name, value] of Object.entries(response)) {
+        params[name] = value;
+    }
+};
 
 module.exports = AppConfigLoader;
